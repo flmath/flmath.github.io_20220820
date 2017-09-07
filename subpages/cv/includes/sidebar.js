c40c1a48429a51c575c439799ec2088f7bd41edc
flmath <fulllethalmathematic@gmail.com>
2017-09-07 08:41:32 +0200

    sidebar.js update and printable html

--- subpages/cv/includes/sidebar.js
+++ subpages/cv/includes/sidebar.js
@@ -1,6 +1,25 @@
 $('#nav').affix({
     offset: {     
-      top: $('#nav').offset().top,
-      bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
+	top: $('#nav').offset().top,
+	bottom: $('.test_body').outerHeight(true) +40
     }
+});
+
+
+$(window).on("load",function() {
+    //console.log(sessionStorage.getItem('current_tag'));
+    window.scrollTo(0, 0);
+});
+
+$('#nav').on("activate.bs.scrollspy", function () {
+    var active_elem = $(".nav li.active a"); 
+    var current_url = active_elem.attr('href');
+    var tmp_id =  current_url.substring(current_url.indexOf("#"))
+    // var active_section = $(".test_body " + tmp_id);
+    // console.log(active_section);
+    // var current_tag_offset = active_section.position().top;  
+    // sessionStorage.setItem('current_tag_offset',current_tag_offset);
+    // console.log(current_tag_offset);
+    // sessionStorage.setItem('current_tag',tmp_id);
+    window.location.href = tmp_id;
 });
