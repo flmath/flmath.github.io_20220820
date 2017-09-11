6cab30c241971c2491e305181bf6b95970587a0d
flmath <fulllethalmathematic@gmail.com>
2017-09-11 05:32:08 +0200

    Refactoring

--- subpages/cv/includes/sidebar.js
+++ subpages/cv/includes/sidebar.js
@@ -5,21 +5,17 @@ $('#nav').affix({
     }
 });
 
-
-$(window).on("load",function() {
-    //console.log(sessionStorage.getItem('current_tag'));
+$(document).ready(function() {
     window.scrollTo(0, 0);
+    var current_url = window.location.href;
+    var hash_position = current_url.indexOf("#");
+    if (hash_position>-1)
+    {
+	window.location.href=current_url.substring(0,hash_position);
+    };
 });
 
+
 $('#nav').on("activate.bs.scrollspy", function () {
-    var active_elem = $(".nav li.active a"); 
-    var current_url = active_elem.attr('href');
-    var tmp_id =  current_url.substring(current_url.indexOf("#"))
-    // var active_section = $(".test_body " + tmp_id);
-    // console.log(active_section);
-    // var current_tag_offset = active_section.position().top;  
-    // sessionStorage.setItem('current_tag_offset',current_tag_offset);
-    // console.log(current_tag_offset);
-    // sessionStorage.setItem('current_tag',tmp_id);
-    window.location.href = tmp_id;
+
 });
