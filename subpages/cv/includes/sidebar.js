9189829c42ebbc63443073ee8388b197b75e959a
flmath <fulllethalmathematic@gmail.com>
2017-09-11 07:11:57 +0200

    no saving solution

--- subpages/cv/includes/sidebar.js
+++ subpages/cv/includes/sidebar.js
@@ -5,17 +5,13 @@ $('#nav').affix({
     }
 });
 
-$(document).ready(function() {
-    window.scrollTo(0, 0);
-    var current_url = window.location.href;
-    var hash_position = current_url.indexOf("#");
-    if (hash_position>-1)
-    {
-	window.location.href=current_url.substring(0,hash_position);
-    };
-});
+
 
 
 $('#nav').on("activate.bs.scrollspy", function () {
+ 
+});
 
+$(window).on("load",function() {
+    window.scrollTo(0, 0);
 });
